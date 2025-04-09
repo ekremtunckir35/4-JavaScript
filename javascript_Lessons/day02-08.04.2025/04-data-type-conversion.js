8f2511ce936007d4d74de72a82609ec7844a180c
EkremTnckr <ekremtunckir@gmail.com>
2025-04-09 17:32:34 +0300

    String literals, comporation operators,logical operators,assignment operators

--- javascript_Lessons/day02-08.04.2025/04-data-type-conversion.js
+++ javascript_Lessons/day02-08.04.2025/04-data-type-conversion.js
@@ -1,3 +1,4 @@
+// Number'a Dönüştürme
 let para = "100";
 let vergi = 10;
 
@@ -10,4 +11,50 @@ console.log(Number(isim) + vergi); // NaN
 
 // Eğer bir data tipini number'a çevirmek istiyorsak Number() veya + işaretini kullanabilir.
 
-console.log( +para + vergi);
+console.log(+para + vergi);
+
+// String'e Dönüştürme
+let barcodeNumber = 9000;
+let extension = 123;
+
+console.log(String(barcodeNumber) + extension);
+// Eğer bir data tipini string'e çevirmek istiyorsak String() veya .toString() kullanılabilir.
+
+console.log("" + barcodeNumber);
+
+let barcodeString = barcodeNumber.toString();
+console.log(barcodeString + extension);
+
+// BigInt'e Dönüştürme
+let x = 150n;
+let y = 5;
+console.log(x + BigInt(y));
+
+// Boolean'a Dönüştürme
+
+// 0, 0n, undefined, null, NaN ve "" Boolean'a dönüştürüldüğünde false getirir. Diğer tüm değerler true getirir.
+
+let number = 10;
+console.log(Boolean(number)); // true
+number -10;
+console.log(Boolean(number)); // true
+number = "Hello";
+console.log(Boolean(number)); // true
+
+let myObj = {};
+console.log("Boş obje: ", (Boolean(myObj))); // true
+let myArr = [];
+console.log("Boş array: ", Boolean(myArr)); // true
+
+number = 0;
+console.log(Boolean(number)); // false
+number = "";
+console.log(Boolean(number)); // false
+number = null;
+console.log(Boolean(number)); // false
+
+let aVariable;
+console.log(Boolean(aVariable)); // false
+console.log(Boolean(3-3)); // false
+console.log(Boolean(NaN)); // false
+
---
+++ javascript_Lessons/day03-09.04.2025/03--comparison-operators.js
@@ -0,0 +1,39 @@
+let x = 50;
+let y = "50";
+let z = 100;
+let a = 50;
+
+console.log(x == y); // true => değerelere bakıp karşılaştırma yapar. Stringi number'a çevirir.
+console.log(x == z); // false
+
+console.log(x === y); // false => çünkü hem data tipine hem de değere bakar
+console.log(typeof(x), typeof(y));
+
+console.log(a == x); // true => hem data tipi hem de değer eşittir.
+console.log(typeof a , typeof x);
+
+
+console.log(x != y); // false => ikisinin de değerine bakar eğer eşitse false verir, değilse true verir
+console.log(x != z); // true
+
+console.log(x !== y); // true
+console.log(a !== x); // false
+
+
+console.log("------------");
+
+console.log(100 > 50); // true
+console.log(100 < 50); // false
+console.log(50 >= 50); // true
+console.log(50 > "40"); // true
+
+// String Number karşılaştırmaları eğer string number'a NaN olarak çevriliyorsa karşılaştırma her zaman false değeri verir.
+console.log(1 < "elma"); // false
+console.log(1 > "elma"); // false
+console.log(1 == "elma"); // false
+
+// String - String Karşılaşırmaları
+console.log("9" > "1000"); // true
+console.log("1000000" > "9"); // false
+
+console.log("elma" > "armut"); // true => ASCII değerlerine bakar.

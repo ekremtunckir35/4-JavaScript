8db72a033c9fd60574566f1a22a192fa61038e84
EkremTnckr <ekremtunckir@gmail.com>
2025-04-11 17:28:44 +0300

    "if,if else,if elseif else,switch

---
+++ javascript_Lessons/day05-11.04.2025/04-if-elseif-else.js
@@ -0,0 +1,12 @@
+ // Kaç basamklı olduğunu
+let number = 500;
+
+if(number >= 1000){
+    console.log("4 basamklı");
+} else if(number >= 100){
+    console.log("3 basamaklı");
+} else if(number >= 10){
+    console.log("2 basamklı");
+} else {
+    console.log("1 basamklı");
+}
---
+++ javascript_Lessons/day05-11.04.2025/05-switch.js
@@ -0,0 +1,16 @@
+let expression = "armut";
+
+switch (expression) {
+  case "armut":
+    console.log("Armut seçildi");
+    break;
+  case "muz":
+    console.log("Muz seçildi");
+    break;
+  case "elma":
+    console.log("Elma seçildi");
+    break;
+    default: 
+    console.log("İstediğiniz ürün yok.");
+    // break;
+}

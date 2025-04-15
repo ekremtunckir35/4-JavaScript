18c1c8dd0742fd131f3342f3cc539d2f470eb11d
EkremTnckr <ekremtunckir@gmail.com>
2025-04-16 01:25:39 +0300

    for loop,while loop,do while loop

---
+++ B293_Javascript/day07-15.04.2025/01-for-loop.js
@@ -0,0 +1,27 @@
+//Konsolda 10 kere Merhaba yazsini yazdir
+
+console.log("Merhaba");
+console.log("Merhaba");
+console.log("Merhaba");
+console.log("Merhaba");
+console.log("Merhaba");
+console.log("Merhaba");
+console.log("Merhaba");
+console.log("Merhaba");
+console.log("Merhaba");
+console.log("Merhaba");
+
+//Yukardaki kodu for loop ile yazdirin
+//baslangic degeri    sartli ifade   degisim(i = i +1)
+for (let i = 0;      i < 10;         i++) {
+    console.log("Merhaba");
+}
+
+//i index anlamina gelir 
+//index -->bulundugu yer anlamina gelir.
+//i 0'dan baslar ve 10'a kadar gider.
+
+console.log(index);
+
+console.log(`Merhaba ${i}`); // 10
+// console.log(i); // 10

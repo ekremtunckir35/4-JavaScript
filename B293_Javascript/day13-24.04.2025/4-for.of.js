c5eb8fc6d0bc960a895dd04ee50db3a667f7d103
EkremTnckr <ekremtunckir@gmail.com>
2025-04-24 17:29:52 +0300

    array access method,array for iteration,for in,for of

---
+++ B293_Javascript/day13-24.04.2025/4-for.of.js
@@ -0,0 +1,10 @@
+const veggies =["kabak","patates","patlican","pirasa"];
+
+for (let veg of veggies) {
+    console.log(veg);
+}
+
+const myStr = "Hello World!";
+for (let char of myStr) {
+    console.log(char);
+}
